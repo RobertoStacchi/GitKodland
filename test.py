ea2efba84d46b799c7be89f5130375570726d02e
RobertoStacchi <wellroby@gmail.com>
2024-03-06 15:33:55 +0100

    Creato un programma che crea delle password casuali con un numero di caratteri a scelta.

---
+++ test.py
@@ -0,0 +1,7 @@
+import random
+import time
+
+caratteriSpec = ["<|>+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"]
+print(caratteriSpec)
+numero_caratteri = int(input("Inserisci il numero di caratteri desiderato per la password: "))
+print(numero_caratteri)
