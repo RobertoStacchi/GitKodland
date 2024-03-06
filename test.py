15f1732b5ef4d8357785ef82321d3bdf11782e11
RobertoStacchi <wellroby@gmail.com>
2024-03-06 17:58:16 +0100

    Aggiunta di un creatore di password.

--- test.py
+++
@@ -1,7 +0,0 @@
-import random
-import time
-
-caratteriSpec = ["<|>+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"]
-print(caratteriSpec)
-numero_caratteri = int(input("Inserisci il numero di caratteri desiderato per la password: "))
-print(numero_caratteri)
