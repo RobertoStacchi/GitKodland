a5cf5ae4452d34d35c38496cfa18d2bb404e0664
RobertoStacchi <wellroby@gmail.com>
2024-03-08 11:17:38 +0100

    Creato botDs.py
    All'interno di questo file si trova il codice di un bot di discord che risponde automaticamente a dei messaggi di altri utenti, tramite la sintassi "client" della libreria discord.

---
+++ botDs.py
@@ -0,0 +1,51 @@
+
+import discord
+
+# la variabile intents contiene i permessi al bot
+intents = discord.Intents.default()
+# abilita il permesso a leggere i contenuti dei messaggi
+intents.message_content = True
+# crea un bot e passa gli indents
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Abbiamo fatto l\'accesso come {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$ciao'):
+        await message.channel.send("Ciao!")
+    elif message.content.startswith('$arrivederci'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("import discord")
+
+# la variabile intents contiene i permessi al bot
+intents = discord.Intents.default()
+# abilita il permesso a leggere i contenuti dei messaggi
+intents.message_content = True
+# crea un bot e passa gli indents
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Abbiamo fatto l\'accesso come {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$ciao'):
+        await message.channel.send("Ciao!")
+    elif message.content.startswith('$arrivederci'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+#Token del bot di Discord
+client.run("token")
